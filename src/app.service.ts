4452f40063cc907bc6c7d800ccc2829db03a9ac5
Julian Alvarez <juliansnei@gmail.com>
2024-07-11 15:29:13 -0500

    new failure

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    const saludo: string = 'hello';
+    const nombre: string = 'julian';
     return 'Hello World!';
   }
 }
